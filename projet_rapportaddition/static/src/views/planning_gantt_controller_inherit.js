4469279403c6c6bcdcd51713cc405dc8d77bf4cc
Hichem Nehaoua <hne@qorelis.com>
2026-05-07 16:46:47 +0100

    add decharge report

--- projet_rapportaddition/static/src/views/planning_gantt_controller_inherit.js
+++ projet_rapportaddition/static/src/views/planning_gantt_controller_inherit.js
@@ -7,38 +7,50 @@ import { _t } from "@web/core/l10n/translation";
 patch(PlanningGanttRenderer.prototype, {
     async getPopoverProps(pill) {
         const popoverProps = await super.getPopoverProps(...arguments);
+        let is_employee = pill.record
 
-        // ═══ زر Ordre de Mission ═══
-        popoverProps.buttons.push({
-            text: _t("Print"),
-            class: "btn btn-sm btn-secondary",
-            onClick: async () => {
-                await this.env.services.action.doAction({
-                    type: "ir.actions.report",
-                    report_type: "qweb-pdf",
-                    report_name: "projet_rapportaddition.report_planning_slot_document",
-                    res_model: "planning.slot",
-                    res_ids: [pill.record.id],
-                    context: { active_ids: [pill.record.id] },
-                });
-            },
-        });
 
-        // ═══ Décharge 
-        popoverProps.buttons.push({
-            text: _t("Décharge"),
-            class: "btn btn-sm btn-secondary",
-            onClick: async () => {
-                await this.env.services.action.doAction({
-                    type: "ir.actions.report",
-                    report_type: "qweb-pdf",
-                    report_name: "projet_rapportaddition.report_decharge_document",
-                    res_model: "planning.slot",
-                    res_ids: [pill.record.id],
-                    context: { active_ids: [pill.record.id] },
-                });
-            },
-        });
+        if (is_employee.employee_id === false) {
+
+            popoverProps.buttons.push({
+                text: _t("Décharge"),
+                class: "btn btn-sm btn-secondary",
+                onClick: async () => {
+                    await this.env.services.action.doAction({
+                        type: "ir.actions.report",
+                        report_type: "qweb-pdf",
+                        report_name: "projet_rapportaddition.report_decharge_document",
+                        res_model: "planning.slot",
+                        res_ids: [pill.record.id],
+                        context: { active_ids: [pill.record.id] },
+                    });
+                },
+            });
+
+
+
+
+        } else {
+
+
+            popoverProps.buttons.push({
+                text: _t("Print"),
+                class: "btn btn-sm btn-secondary",
+                onClick: async () => {
+                    await this.env.services.action.doAction({
+                        type: "ir.actions.report",
+                        report_type: "qweb-pdf",
+                        report_name: "projet_rapportaddition.report_planning_slot_document",
+                        res_model: "planning.slot",
+                        res_ids: [pill.record.id],
+                        context: { active_ids: [pill.record.id] },
+                    });
+                },
+            });
+
+
+
+        }
 
         return popoverProps;
     },
